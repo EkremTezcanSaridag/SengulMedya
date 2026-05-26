a6f8fefcaa346ac4ac37c21c818267720cdf3048
ekremTezcanSaridag <ekremtezcansaridag@anadolu.edu.tr>
2026-05-27 01:55:29 +0300

    galeri sayfası eklendi ve buton hataları düzeltildi

--- js/app.js
+++ js/app.js
@@ -1,84 +1,14 @@
-const images = [
-    'images/anasayfafoto.jpg',
-    'images/resim2.jpg',
-    'images/anasayfafoto.jpg',
-    'images/arkaplan.png',
-    'images/arkaplan.png',
-    'images/arkaplan.png',
-
-];
-
-
-let mainimg = document.querySelector(".slider_img")
-let sag_ok = document.querySelector(".right")
-let sol_ok = document.querySelector(".left")
-
-let imgsayac = 0;
-let resimslider
-
-
-if (mainimg && sag_ok && sol_ok) {
-    sag_ok.addEventListener("click", function () {
-        clearInterval(resimslider);
-        if (imgsayac < images.length - 1) {
-
-            imgsayac += 1;
-            mainimg.src = images[imgsayac];
-        }
-        else {
-            imgsayac = 0;
-            mainimg.src = images[imgsayac];
-        }
-        slideinterval();
-    })
-
-
-    sol_ok.addEventListener("click", function () {
-        clearInterval(resimslider);
-        if (imgsayac > 0) {
-
-            imgsayac -= 1;
-            mainimg.src = images[imgsayac];
-        }
-        else {
-            imgsayac = images.length - 1;
-            mainimg.src = images[imgsayac];
-        }
-        slideinterval();
-
-    })
-
-
-    function slideinterval() {
-        resimslider = setInterval(function () {
-
-            if (imgsayac < images.length - 1) {
-                imgsayac += 1;
-                mainimg.src = images[imgsayac];
-            }
-            else {
-                imgsayac = 0;
-                mainimg.src = images[imgsayac];
-            }
-
-
-        }, 5000)
-    }
-
-    slideinterval();
-}
-
 // Hamburger Menu Logic
 const mobileMenu = document.getElementById('mobile-menu');
 const navbar = document.querySelector('.navbar');
 
-if(mobileMenu) {
+if (mobileMenu) {
     mobileMenu.addEventListener('click', () => {
         navbar.classList.toggle('active');
-        
+
         // Change icon to X when open
         const icon = mobileMenu.querySelector('i');
-        if(navbar.classList.contains('active')) {
+        if (navbar.classList.contains('active')) {
             icon.classList.remove('fa-bars');
             icon.classList.add('fa-xmark');
         } else {
